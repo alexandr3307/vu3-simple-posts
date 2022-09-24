320ae5206fa5de6fc3f1f3b1c85e818146874a65
alex <a.landar@doctis.ru>
2022-09-24 20:26:45 +0300

    create main components, add ui library. Project is ready to create and show posts

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <template>
-    ЫЫЫ
+  <div class="app">
+    <PostForm @create="createPost" />
+    <PostList v-bind:posts="posts" />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "App"
-    }
+import PostList from "@/components/PostList";
+import PostForm from "@/components/PostForm";
+export default {
+  name: "App",
+  components: { PostForm, PostList },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "java", body: "Описание" },
+        { id: 2, title: "java", body: "Описание" },
+        { id: 3, title: "java", body: "Описание" },
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+    },
+  },
+};
 </script>
 
-<style scoped>
-
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <MyInput
+      v-model="post.title"
+      type="text"
+      placeholder="Название"
+    />
+    <MyInput
+      v-model="post.body"
+      type="text"
+      placeholder="Описание"
+    />
+    <MyButton class="btn-end" @click="createPost">Создать</MyButton>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "PostForm",
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post);
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+.btn-end {
+  align-self: flex-end;
+  margin-top: 15px;
+}
+</style>
